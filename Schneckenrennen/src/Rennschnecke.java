3ee4ee701ffc1214ae944b32180c14d28acf7d31
Kappicht.Jannik <Kappicht.Jannik@DESKTOP-F25JDAB.2631.skool.net>
2018-10-18 09:12:35 +0200

    Initial commit

--- Schneckenrennen/src/Rennschnecke.java
+++ Schneckenrennen/src/Rennschnecke.java
@@ -1,4 +1,27 @@
+import java.util.Random;
 
 public class Rennschnecke {
 
+  String name;
+  String rasse;
+  int vmax;
+  int strecke;
+  
+  public Rennschnecke(String name, String rasse, int vmax){
+	  this.name=name;
+	  this.rasse=rasse;
+	  this.vmax=vmax;
+	  this.strecke=strecke;
+  }
+
+ public void krieche(){
+	   
+  Random ran = new Random();
+  strecke = strecke+ran.nextInt(vmax +1);
+ }
+ 
+ public String toString(){
+	 return "Name: "+name+"\n Rasse: "+rasse+"\n Höchstgeschwindigkeit: "+vmax+"\n Geschleimte Strecke: "+strecke;
+ }
+ 
 }
---
+++ Schneckenrennen/src/Schneckenrennnen.java
@@ -0,0 +1,24 @@
+
+public class Schneckenrennnen {
+
+	public static void main(String[] args) {
+		
+		Rennschnecke s1 = new Rennschnecke("Hasi", "Glitscho", 3);
+		Rennschnecke s2 = new Rennschnecke("Baerchen", "Rasanto", 5);
+		Rennschnecke s3 = new Rennschnecke("Hund", "Haarig", 4);
+		Rennschnecke s4 = new Rennschnecke("Vogel", "Speed", 10);
+		Rennschnecke s5 = new Rennschnecke("Jet", "fastest", 100);
+	 
+	 s1.krieche();
+	 System.out.println(s1);
+	 s2.krieche();
+	 System.out.println(s2);
+	 s3.krieche();
+	 System.out.println(s3);
+	 s4.krieche();
+	 System.out.println(s4);
+	 s5.krieche();
+	 System.out.println(s5);
+	}
+
+}
